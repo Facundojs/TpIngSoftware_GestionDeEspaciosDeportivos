256b06e52a62b8935ac25f89e84464aebcfc3b7f
Facundo Serrano <facuserra2002@gmail.com>
2026-02-28 10:30:16 -0300

    fix cmp

--- Service/Facade/Extension/TranslationsExtension.cs
+++ Service/Facade/Extension/TranslationsExtension.cs
@@ -1,10 +1,11 @@
 using System;
+using Domain.Enums;
 
 namespace Service.Facade.Extension
 {
     public static class TranslationsExtension
     {
-        public static string Translate(this Domain.Enums.Translations key)
+        public static string Translate(this Translations key)
         {
             return Service.Logic.LanguageLogic.Translate(key.ToString());
         }
