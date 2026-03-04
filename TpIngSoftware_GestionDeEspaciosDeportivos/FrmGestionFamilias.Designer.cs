1f6c40d8b67e9ce9fefc1ba8a7bcd4166c144fcd
Facundo Serrano <facundo.serrano@payclip.com>
2026-03-03 23:43:44 -0300

    ux

--- TpIngSoftware_GestionDeEspaciosDeportivos/FrmGestionFamilias.Designer.cs
+++ TpIngSoftware_GestionDeEspaciosDeportivos/FrmGestionFamilias.Designer.cs
@@ -28,9 +28,10 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstFamilias = new System.Windows.Forms.ListBox();
+            this.tvFamilias = new System.Windows.Forms.TreeView();
             this.lstPatentes = new System.Windows.Forms.CheckedListBox();
             this.lstSubFamilias = new System.Windows.Forms.CheckedListBox();
+            this.tvPreview = new System.Windows.Forms.TreeView();
             this.txtNombreFamilia = new System.Windows.Forms.TextBox();
             this.lblNombreFamilia = new System.Windows.Forms.Label();
             this.btnCrear = new System.Windows.Forms.Button();
@@ -39,76 +40,85 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
             this.lblFamilias = new System.Windows.Forms.Label();
             this.lblPatentes = new System.Windows.Forms.Label();
             this.lblSubFamilias = new System.Windows.Forms.Label();
+            this.lblPreview = new System.Windows.Forms.Label();
             this.SuspendLayout();
             //
-            // lstFamilias
+            // tvFamilias
             //
-            this.lstFamilias.FormattingEnabled = true;
-            this.lstFamilias.ItemHeight = 16;
-            this.lstFamilias.Location = new System.Drawing.Point(12, 33);
-            this.lstFamilias.Name = "lstFamilias";
-            this.lstFamilias.Size = new System.Drawing.Size(180, 324);
-            this.lstFamilias.TabIndex = 0;
-            this.lstFamilias.SelectedIndexChanged += new System.EventHandler(this.lstFamilias_SelectedIndexChanged);
+            this.tvFamilias.Location = new System.Drawing.Point(12, 33);
+            this.tvFamilias.Name = "tvFamilias";
+            this.tvFamilias.Size = new System.Drawing.Size(190, 265);
+            this.tvFamilias.TabIndex = 0;
+            this.tvFamilias.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvFamilias_AfterSelect);
             //
             // lstPatentes
             //
             this.lstPatentes.FormattingEnabled = true;
-            this.lstPatentes.Location = new System.Drawing.Point(205, 33);
+            this.lstPatentes.Location = new System.Drawing.Point(207, 33);
             this.lstPatentes.Name = "lstPatentes";
-            this.lstPatentes.Size = new System.Drawing.Size(200, 327);
+            this.lstPatentes.Size = new System.Drawing.Size(185, 265);
             this.lstPatentes.TabIndex = 1;
+            this.lstPatentes.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lstPatentes_ItemCheck);
             //
             // lstSubFamilias
             //
             this.lstSubFamilias.FormattingEnabled = true;
-            this.lstSubFamilias.Location = new System.Drawing.Point(415, 33);
+            this.lstSubFamilias.Location = new System.Drawing.Point(397, 33);
             this.lstSubFamilias.Name = "lstSubFamilias";
-            this.lstSubFamilias.Size = new System.Drawing.Size(200, 327);
+            this.lstSubFamilias.Size = new System.Drawing.Size(185, 265);
             this.lstSubFamilias.TabIndex = 2;
+            this.lstSubFamilias.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lstSubFamilias_ItemCheck);
             //
-            // txtNombreFamilia
+            // tvPreview
             //
-            this.txtNombreFamilia.Location = new System.Drawing.Point(630, 50);
-            this.txtNombreFamilia.Name = "txtNombreFamilia";
-            this.txtNombreFamilia.Size = new System.Drawing.Size(180, 22);
-            this.txtNombreFamilia.TabIndex = 3;
+            this.tvPreview.Location = new System.Drawing.Point(587, 33);
+            this.tvPreview.Name = "tvPreview";
+            this.tvPreview.Size = new System.Drawing.Size(231, 265);
+            this.tvPreview.TabIndex = 3;
+            this.tvPreview.LabelEdit = false;
             //
             // lblNombreFamilia
             //
             this.lblNombreFamilia.AutoSize = true;
-            this.lblNombreFamilia.Location = new System.Drawing.Point(627, 30);
+            this.lblNombreFamilia.Location = new System.Drawing.Point(12, 314);
             this.lblNombreFamilia.Name = "lblNombreFamilia";
             this.lblNombreFamilia.Size = new System.Drawing.Size(61, 16);
             this.lblNombreFamilia.TabIndex = 4;
             this.lblNombreFamilia.Text = "Nombre:";
             //
+            // txtNombreFamilia
+            //
+            this.txtNombreFamilia.Location = new System.Drawing.Point(75, 311);
+            this.txtNombreFamilia.Name = "txtNombreFamilia";
+            this.txtNombreFamilia.Size = new System.Drawing.Size(160, 22);
+            this.txtNombreFamilia.TabIndex = 5;
+            //
             // btnCrear
             //
-            this.btnCrear.Location = new System.Drawing.Point(630, 90);
+            this.btnCrear.Location = new System.Drawing.Point(245, 309);
             this.btnCrear.Name = "btnCrear";
-            this.btnCrear.Size = new System.Drawing.Size(100, 30);
-            this.btnCrear.TabIndex = 5;
+            this.btnCrear.Size = new System.Drawing.Size(85, 28);
+            this.btnCrear.TabIndex = 6;
             this.btnCrear.Text = "Crear";
             this.btnCrear.UseVisualStyleBackColor = true;
             this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
             //
             // btnActualizar
             //
-            this.btnActualizar.Location = new System.Drawing.Point(630, 130);
+            this.btnActualizar.Location = new System.Drawing.Point(338, 309);
             this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(100, 30);
-            this.btnActualizar.TabIndex = 6;
+            this.btnActualizar.Size = new System.Drawing.Size(85, 28);
+            this.btnActualizar.TabIndex = 7;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = true;
             this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             //
             // btnEliminar
             //
-            this.btnEliminar.Location = new System.Drawing.Point(630, 170);
+            this.btnEliminar.Location = new System.Drawing.Point(431, 309);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(100, 30);
-            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Size = new System.Drawing.Size(85, 28);
+            this.btnEliminar.TabIndex = 8;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
@@ -119,43 +129,54 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
             this.lblFamilias.Location = new System.Drawing.Point(12, 14);
             this.lblFamilias.Name = "lblFamilias";
             this.lblFamilias.Size = new System.Drawing.Size(61, 16);
-            this.lblFamilias.TabIndex = 8;
+            this.lblFamilias.TabIndex = 9;
             this.lblFamilias.Text = "Familias";
             //
             // lblPatentes
             //
             this.lblPatentes.AutoSize = true;
-            this.lblPatentes.Location = new System.Drawing.Point(205, 14);
+            this.lblPatentes.Location = new System.Drawing.Point(207, 14);
             this.lblPatentes.Name = "lblPatentes";
             this.lblPatentes.Size = new System.Drawing.Size(61, 16);
-            this.lblPatentes.TabIndex = 9;
+            this.lblPatentes.TabIndex = 10;
             this.lblPatentes.Text = "Patentes";
             //
             // lblSubFamilias
             //
             this.lblSubFamilias.AutoSize = true;
-            this.lblSubFamilias.Location = new System.Drawing.Point(415, 14);
+            this.lblSubFamilias.Location = new System.Drawing.Point(397, 14);
             this.lblSubFamilias.Name = "lblSubFamilias";
             this.lblSubFamilias.Size = new System.Drawing.Size(80, 16);
-            this.lblSubFamilias.TabIndex = 10;
+            this.lblSubFamilias.TabIndex = 11;
             this.lblSubFamilias.Text = "Sub-Familias";
+            //
+            // lblPreview
+            //
+            this.lblPreview.AutoSize = true;
+            this.lblPreview.Location = new System.Drawing.Point(587, 14);
+            this.lblPreview.Name = "lblPreview";
+            this.lblPreview.Size = new System.Drawing.Size(70, 16);
+            this.lblPreview.TabIndex = 12;
+            this.lblPreview.Text = "Vista Previa";
             //
             // FrmGestionFamilias
             //
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(830, 380);
+            this.ClientSize = new System.Drawing.Size(830, 350);
+            this.Controls.Add(this.lblPreview);
             this.Controls.Add(this.lblSubFamilias);
             this.Controls.Add(this.lblPatentes);
             this.Controls.Add(this.lblFamilias);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnActualizar);
             this.Controls.Add(this.btnCrear);
-            this.Controls.Add(this.lblNombreFamilia);
             this.Controls.Add(this.txtNombreFamilia);
+            this.Controls.Add(this.lblNombreFamilia);
+            this.Controls.Add(this.tvPreview);
             this.Controls.Add(this.lstSubFamilias);
             this.Controls.Add(this.lstPatentes);
-            this.Controls.Add(this.lstFamilias);
+            this.Controls.Add(this.tvFamilias);
             this.Name = "FrmGestionFamilias";
             this.Text = "Gestión de Familias";
             this.Load += new System.EventHandler(this.FrmGestionFamilias_Load);
@@ -166,9 +187,10 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
 
         #endregion
 
-        private System.Windows.Forms.ListBox lstFamilias;
+        private System.Windows.Forms.TreeView tvFamilias;
         private System.Windows.Forms.CheckedListBox lstPatentes;
         private System.Windows.Forms.CheckedListBox lstSubFamilias;
+        private System.Windows.Forms.TreeView tvPreview;
         private System.Windows.Forms.TextBox txtNombreFamilia;
         private System.Windows.Forms.Label lblNombreFamilia;
         private System.Windows.Forms.Button btnCrear;
@@ -177,5 +199,6 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
         private System.Windows.Forms.Label lblFamilias;
         private System.Windows.Forms.Label lblPatentes;
         private System.Windows.Forms.Label lblSubFamilias;
+        private System.Windows.Forms.Label lblPreview;
     }
 }
--- TpIngSoftware_GestionDeEspaciosDeportivos/FrmGestionFamilias.cs
+++ TpIngSoftware_GestionDeEspaciosDeportivos/FrmGestionFamilias.cs
@@ -15,6 +15,7 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
     {
         private UsuarioDTO _usuario;
         private PermisosService _permisosService;
+        private Familia _familiaSeleccionada;
 
         public FrmGestionFamilias(UsuarioDTO usuario)
         {
@@ -30,6 +31,7 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
             lblFamilias.Text = "Familias";
             lblPatentes.Text = "Patentes";
             lblSubFamilias.Text = Translations.LBL_SUBFAMILIAS.Translate();
+            lblPreview.Text = "Vista Previa";
             lblNombreFamilia.Text = Translations.LBL_NOMBRE.Translate();
             btnCrear.Text = Translations.BTN_CREAR.Translate();
             btnActualizar.Text = Translations.BTN_ACTUALIZAR.Translate();
@@ -45,7 +47,6 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
         private void ApplyPermissions()
         {
             if (_usuario == null) return;
-
             if (!_usuario.TienePermiso(PermisoKeys.PermisoAsignar))
             {
                 MessageBox.Show(Translations.MSG_NO_PERM_LIST.Translate());
@@ -56,68 +57,173 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
         private void LoadData()
         {
             LoadPatentes();
-            LoadFamilias();
-            LoadSubFamilias(null);
+            LoadSubFamilias(null, new HashSet<Guid>());
+            RefreshHierarchy();
         }
 
-        private void LoadFamilias()
+        // Rebuilds tvFamilias showing only root Familias and their full subtrees.
+        private void RefreshHierarchy()
         {
-            lstFamilias.Items.Clear();
-            var familias = _permisosService.GetAllFamilias();
-            foreach (var fam in familias)
-                lstFamilias.Items.Add(fam);
-            lstFamilias.DisplayMember = "Nombre";
+            tvFamilias.BeginUpdate();
+            tvFamilias.Nodes.Clear();
+            var todas = _permisosService.GetAllFamilias();
+            foreach (var fam in GetRootFamilias(todas))
+            {
+                var node = new TreeNode(fam.Nombre) { Tag = fam };
+                AddNodosRecursivos(node, fam.Accesos);
+                tvFamilias.Nodes.Add(node);
+            }
+            tvFamilias.ExpandAll();
+            tvFamilias.EndUpdate();
+        }
+
+        private void AddNodosRecursivos(TreeNode parent, IList<Acceso> accesos)
+        {
+            foreach (var acceso in accesos)
+            {
+                var node = new TreeNode(acceso.Nombre) { Tag = acceso };
+                if (acceso is Familia sub)
+                    AddNodosRecursivos(node, sub.Accesos);
+                parent.Nodes.Add(node);
+            }
+        }
+
+        // Returns Familias that do not appear as a child inside any other Familia's subtree.
+        private List<Familia> GetRootFamilias(List<Familia> todas)
+        {
+            var childIds = new HashSet<Guid>();
+            foreach (var f in todas)
+                CollectChildFamiliaIds(f.Accesos, childIds);
+            return todas.Where(f => !childIds.Contains(f.Id)).ToList();
+        }
+
+        private void CollectChildFamiliaIds(IList<Acceso> accesos, HashSet<Guid> childIds)
+        {
+            foreach (var acceso in accesos)
+            {
+                if (acceso is Familia f)
+                {
+                    childIds.Add(f.Id);
+                    CollectChildFamiliaIds(f.Accesos, childIds);
+                }
+            }
+        }
+
+        // Returns IDs of Familias that have familiaId somewhere in their descendant tree,
+        // i.e. adding them as children of familiaId would create a cycle.
+        private HashSet<Guid> GetAncestorIds(Guid familiaId, List<Familia> todas)
+        {
+            var ancestors = new HashSet<Guid>();
+            foreach (var f in todas)
+            {
+                if (f.Id != familiaId && ContainsDescendant(f, familiaId, new HashSet<Guid>()))
+                    ancestors.Add(f.Id);
+            }
+            return ancestors;
+        }
+
+        private bool ContainsDescendant(Familia familia, Guid targetId, HashSet<Guid> visited)
+        {
+            if (!visited.Add(familia.Id)) return false;
+            foreach (var acceso in familia.Accesos)
+            {
+                if (acceso.Id == targetId) return true;
+                if (acceso is Familia sub && ContainsDescendant(sub, targetId, visited)) return true;
+            }
+            return false;
         }
 
         private void LoadPatentes()
         {
             lstPatentes.Items.Clear();
-            var patentes = _permisosService.GetAllPatentes();
-            foreach (var pat in patentes)
+            foreach (var pat in _permisosService.GetAllPatentes())
                 lstPatentes.Items.Add(pat);
             lstPatentes.DisplayMember = "Nombre";
         }
 
-        private void LoadSubFamilias(Guid? excludeId)
+        private void LoadSubFamilias(Guid? excludeId, HashSet<Guid> ancestorIds, List<Familia> allFamilias = null)
         {
             lstSubFamilias.Items.Clear();
-            var familias = _permisosService.GetAllFamilias();
+            var familias = allFamilias ?? _permisosService.GetAllFamilias();
             foreach (var fam in familias)
             {
                 if (excludeId.HasValue && fam.Id == excludeId.Value) continue;
+                if (ancestorIds.Contains(fam.Id)) continue;
                 lstSubFamilias.Items.Add(fam);
             }
             lstSubFamilias.DisplayMember = "Nombre";
         }
 
-        private void lstFamilias_SelectedIndexChanged(object sender, EventArgs e)
+        private void RefreshPreview()
         {
-            if (lstFamilias.SelectedItem is Familia fam)
+            tvPreview.BeginUpdate();
+            tvPreview.Nodes.Clear();
+            string nombre = string.IsNullOrWhiteSpace(txtNombreFamilia.Text) ? "..." : txtNombreFamilia.Text;
+            var root = new TreeNode(nombre);
+            foreach (var item in lstPatentes.CheckedItems)
             {
-                txtNombreFamilia.Text = fam.Nombre;
-                LoadSubFamilias(fam.Id);
-
-                for (int i = 0; i < lstPatentes.Items.Count; i++)
+                if (item is Patente pat)
+                    root.Nodes.Add(new TreeNode(pat.Nombre));
+            }
+            foreach (var item in lstSubFamilias.CheckedItems)
+            {
+                if (item is Familia sub)
                 {
-                    var pat = lstPatentes.Items[i] as Patente;
-                    lstPatentes.SetItemChecked(i, fam.Accesos.Any(x => x.Id == pat.Id));
-                }
-
-                for (int i = 0; i < lstSubFamilias.Items.Count; i++)
-                {
-                    var sub = lstSubFamilias.Items[i] as Familia;
-                    lstSubFamilias.SetItemChecked(i, fam.Accesos.Any(x => x.Id == sub.Id));
+                    var subNode = new TreeNode(sub.Nombre) { Tag = sub };
+                    AddNodosRecursivos(subNode, sub.Accesos);
+                    root.Nodes.Add(subNode);
                 }
             }
-            else
+            tvPreview.Nodes.Add(root);
+            tvPreview.ExpandAll();
+            tvPreview.EndUpdate();
+        }
+
+        private void ClearEditor()
+        {
+            _familiaSeleccionada = null;
+            txtNombreFamilia.Text = "";
+            for (int i = 0; i < lstPatentes.Items.Count; i++)
+                lstPatentes.SetItemChecked(i, false);
+            LoadSubFamilias(null, new HashSet<Guid>());
+            tvPreview.Nodes.Clear();
+            tvFamilias.SelectedNode = null;
+        }
+
+        private void tvFamilias_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (!(e.Node?.Tag is Familia fam)) return;
+
+            _familiaSeleccionada = fam;
+            txtNombreFamilia.Text = fam.Nombre;
+
+            var todas = _permisosService.GetAllFamilias();
+            var ancestorIds = GetAncestorIds(fam.Id, todas);
+            LoadSubFamilias(fam.Id, ancestorIds, todas);
+
+            for (int i = 0; i < lstPatentes.Items.Count; i++)
             {
-                txtNombreFamilia.Text = "";
-                LoadSubFamilias(null);
-                for (int i = 0; i < lstPatentes.Items.Count; i++)
-                    lstPatentes.SetItemChecked(i, false);
-                for (int i = 0; i < lstSubFamilias.Items.Count; i++)
-                    lstSubFamilias.SetItemChecked(i, false);
+                var pat = lstPatentes.Items[i] as Patente;
+                lstPatentes.SetItemChecked(i, fam.Accesos.Any(x => x.Id == pat.Id));
             }
+            for (int i = 0; i < lstSubFamilias.Items.Count; i++)
+            {
+                var sub = lstSubFamilias.Items[i] as Familia;
+                lstSubFamilias.SetItemChecked(i, fam.Accesos.Any(x => x.Id == sub.Id));
+            }
+
+            RefreshPreview();
+        }
+
+        // ItemCheck fires before the state updates; BeginInvoke defers RefreshPreview until after.
+        private void lstPatentes_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (!IsDisposed) this.BeginInvoke(new Action(RefreshPreview));
+        }
+
+        private void lstSubFamilias_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (!IsDisposed) this.BeginInvoke(new Action(RefreshPreview));
         }
 
         private void btnCrear_Click(object sender, EventArgs e)
@@ -130,29 +236,22 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
 
             try
             {
-                var nuevaFamilia = new Familia
-                {
-                    Id = Guid.NewGuid(),
-                    Nombre = txtNombreFamilia.Text
-                };
-
+                var nuevaFamilia = new Familia { Id = Guid.NewGuid(), Nombre = txtNombreFamilia.Text };
                 foreach (var item in lstPatentes.CheckedItems)
                 {
-                    if (item is Patente pat)
-                        nuevaFamilia.Agregar(pat);
+                    if (item is Patente pat) nuevaFamilia.Agregar(pat);
                 }
-
                 foreach (var item in lstSubFamilias.CheckedItems)
                 {
-                    if (item is Familia sub)
-                        nuevaFamilia.Agregar(sub);
+                    if (item is Familia sub) nuevaFamilia.Agregar(sub);
                 }
 
                 _permisosService.CrearFamilia(nuevaFamilia);
                 _permisosService.GuardarFamilia(nuevaFamilia);
 
                 MessageBox.Show(Translations.MSG_SUCCESS.Translate());
-                LoadFamilias();
+                RefreshHierarchy();
+                ClearEditor();
             }
             catch (Exception ex)
             {
@@ -162,65 +261,61 @@ namespace TpIngSoftware_GestionDeEspaciosDeportivos
 
         private void btnActualizar_Click(object sender, EventArgs e)
         {
-            if (lstFamilias.SelectedItem is Familia fam)
+            if (_familiaSeleccionada == null) return;
+            if (_familiaSeleccionada.Nombre == "Administrador")
             {
-                if (fam.Nombre == "Administrador")
+                MessageBox.Show(Translations.ERR_CANNOT_EDIT_ADMIN.Translate());
+                return;
+            }
+
+            try
+            {
+                _familiaSeleccionada.Nombre = txtNombreFamilia.Text;
+
+                var newAccesos = new List<Acceso>();
+                foreach (var item in lstPatentes.CheckedItems)
                 {
-                    MessageBox.Show(Translations.ERR_CANNOT_EDIT_ADMIN.Translate());
-                    return;
+                    if (item is Patente pat) newAccesos.Add(pat);
+                }
+                foreach (var item in lstSubFamilias.CheckedItems)
+                {
+                    if (item is Familia sub) newAccesos.Add(sub);
                 }
 
-                try
-                {
-                    fam.Nombre = txtNombreFamilia.Text;
+                _familiaSeleccionada.ClearAccesos();
+                foreach (var acceso in newAccesos)
+                    _familiaSeleccionada.Agregar(acceso);
 
-                    var newAccesos = new List<Acceso>();
-                    foreach (var item in lstPatentes.CheckedItems)
-                    {
-                        if (item is Patente pat)
-                            newAccesos.Add(pat);
-                    }
-                    foreach (var item in lstSubFamilias.CheckedItems)
-                    {
-                        if (item is Familia sub)
-                            newAccesos.Add(sub);
-                    }
-
-                    fam.ClearAccesos();
-                    foreach (var acceso in newAccesos)
-                        fam.Agregar(acceso);
-
-                    _permisosService.GuardarFamilia(fam);
-                    MessageBox.Show(Translations.MSG_SUCCESS.Translate());
-                    LoadFamilias();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(Translations.MSG_ERR_GENERIC.Translate() + ex.Message);
-                }
+                _permisosService.GuardarFamilia(_familiaSeleccionada);
+                MessageBox.Show(Translations.MSG_SUCCESS.Translate());
+                RefreshHierarchy();
+                ClearEditor();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Translations.MSG_ERR_GENERIC.Translate() + ex.Message);
             }
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
-            if (lstFamilias.SelectedItem is Familia fam)
+            if (_familiaSeleccionada == null) return;
+            if (_familiaSeleccionada.Nombre == "Administrador")
             {
-                if (fam.Nombre == "Administrador")
-                {
-                    MessageBox.Show(Translations.ERR_CANNOT_DELETE_ADMIN.Translate());
-                    return;
-                }
+                MessageBox.Show(Translations.ERR_CANNOT_DELETE_ADMIN.Translate());
+                return;
+            }
 
-                try
-                {
-                    _permisosService.EliminarFamilia(fam.Id);
-                    MessageBox.Show(Translations.MSG_SUCCESS.Translate());
-                    LoadFamilias();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(Translations.MSG_ERR_GENERIC.Translate() + ex.Message);
-                }
+            try
+            {
+                _permisosService.EliminarFamilia(_familiaSeleccionada.Id);
+                MessageBox.Show(Translations.MSG_SUCCESS.Translate());
+                RefreshHierarchy();
+                ClearEditor();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Translations.MSG_ERR_GENERIC.Translate() + ex.Message);
             }
         }
     }
